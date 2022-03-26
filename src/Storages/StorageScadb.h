79a11cfbaa27466548142eaba105775b07f4e6e2
yuxianbing <iloveqaz123>
2022-03-26 11:35:15 +0800

    prepare to add scadb

---
+++ src/Storages/StorageScadb.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "config_core.h"
+
+
+#include <base/shared_ptr_helper.h>
+
+#include <Storages/IStorage.h>
+#include <Storages/MySQL/MySQLSettings.h>
+#include <Storages/ExternalDataSourceConfiguration.h>
+#include <mysqlxx/PoolWithFailover.h>
+
+namespace Poco
+{
+class Logger;
+}
+
+namespace DB
+{
+
+/** Implements storage in the Scasql database.
+  * Use ENGINE = scadb(database_name, table_name)
+  * Read only.
+  */
+class StorageScadb final : public shared_ptr_helper<StorageScadb>, public IStorage, WithContext
+{
+    friend struct shared_ptr_helper<StorageScadb>;
+public:
+    StorageScadb(
+        const StorageID & table_id_,
+        mysqlxx::PoolWithFailover && pool_,
+        const std::string & remote_database_name_,
+        const std::string & remote_table_name_,
+        bool replace_query_,
+        const std::string & on_duplicate_clause_,
+        const ColumnsDescription & columns_,
+        const ConstraintsDescription & constraints_,
+        const String & comment,
+        ContextPtr context_,
+        const MySQLSettings & mysql_settings_);
+
+    std::string getName() const override { return "Scasql"; }
+
+    Pipe read(
+        const Names & column_names,
+        const StorageMetadataPtr & /*metadata_snapshot*/,
+        SelectQueryInfo & query_info,
+        ContextPtr context,
+        QueryProcessingStage::Enum processed_stage,
+        size_t max_block_size,
+        unsigned num_streams) override;
+
+    SinkToStoragePtr write(const ASTPtr & query, const StorageMetadataPtr & /*metadata_snapshot*/, ContextPtr context) override;
+
+    static StorageMySQLConfiguration getConfiguration(ASTs engine_args, ContextPtr context_);
+
+private:
+    friend class StorageScasqlSink;
+
+    std::string remote_database_name;
+    std::string remote_table_name;
+    bool replace_query;
+    std::string on_duplicate_clause;
+
+    MySQLSettings mysql_settings;
+
+    mysqlxx::PoolWithFailoverPtr pool;
+
+    Poco::Logger * log;
+};
+
+}
+
